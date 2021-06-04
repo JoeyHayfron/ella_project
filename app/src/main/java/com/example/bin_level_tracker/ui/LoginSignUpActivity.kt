d258bf19ff9cfb9bbe65ab49e011ffdad057a5ce
JoeyHayfron <joeacq81@gmail.com>
2021-06-04 11:12:52 +0000

    Switching PC's

---
+++ app/src/main/java/com/example/bin_level_tracker/ui/LoginSignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bin_level_tracker.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bin_level_tracker.R
+
+class LoginSignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_sign_up)
+    }
+}
